c71a11de7832bea09a387bb72cfb5fc5b695647d
João Matheus Adorni Jinkings <joao.matheusj@dinfor.uniceub>
2024-06-17 21:15:02 -0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
   runApp(MaterialApp(
     theme: ThemeData(
       brightness: Brightness.dark,
